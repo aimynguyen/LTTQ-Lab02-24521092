f1b914f7b8ef1d220a2eeadb07fc9c2598ee7156
Nguyễn Ái My <24521092@gm.uit.edu.vn>
2025-10-30 22:58:42 +0700

    Add files via upload

---
+++ bai04/PhanSo.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai04
+{
+    internal class PhanSo : IComparable<PhanSo>
+    {
+        private int iTuSo;
+        private int iMauSo;
+        public PhanSo()
+        {
+            iTuSo = 0;
+            iMauSo = 1;
+        }
+        public PhanSo(int ts, int ms)
+        {
+            iTuSo = ts;
+            iMauSo = ms;
+        }
+        public void Nhap()
+        {
+           while (true)
+            {
+                Console.Write("tu so: ");
+                if (int.TryParse(Console.ReadLine(), out iTuSo))
+                    break;
+                else
+                    Console.WriteLine("Gia tri khong hop le! Vui long nhap lai!");
+            }
+            while (true)
+            {
+                Console.Write("mau so: ");
+                if (int.TryParse(Console.ReadLine(), out iMauSo) && iMauSo != 0)
+                    break;
+                else
+                    Console.WriteLine("Mau so khong hop le! Vui long nhap lai (yeu cau mau khac 0!)");
+            }          
+
+        }
+
+        public void Xuat()
+        {
+            RutGon();
+            Console.Write($"{iTuSo}/{iMauSo} ");
+        }
+
+        public static int UCLN(int a, int b)
+        {
+            a=Math.Abs(a);
+            b=Math.Abs(b);
+            while (b != 0)
+            {
+                int temp= b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+        public void RutGon()
+        {
+            int ucln=UCLN(iTuSo,iMauSo);
+            iMauSo /=ucln;
+            iTuSo /= ucln;
+
+            //th mau am
+            if (iMauSo < 0)
+            {
+                iMauSo = -iMauSo;
+                iTuSo = -iTuSo;
+            }
+        }
+        public void Print()
+        {
+            
+            if (iTuSo == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+            if (iMauSo == 1)
+            {
+                Console.WriteLine(iTuSo);
+                return;
+            }
+            if (iMauSo == -1)
+            {
+                Console.WriteLine(-iTuSo);
+                return;
+            }
+            Xuat();
+        }
+
+        public static PhanSo operator + (PhanSo a, PhanSo b)
+        {
+            int niMauSo = a.iMauSo * b.iMauSo;
+            int niTuSo = a.iTuSo * b.iMauSo + b.iTuSo * a.iMauSo;
+            PhanSo kq = new PhanSo(niTuSo, niMauSo);
+            kq.RutGon();
+            return kq;
+        }
+
+        public static PhanSo operator -(PhanSo a, PhanSo b)
+        {
+            int niMauSo = a.iMauSo * b.iMauSo;
+            int niTuSo = a.iTuSo * b.iMauSo - b.iTuSo * a.iMauSo;
+            PhanSo kq = new PhanSo(niTuSo, niMauSo);
+            kq.RutGon();
+            return kq;
+        }
+
+        public static PhanSo operator *(PhanSo a, PhanSo b)
+        {
+            int niMauSo = a.iMauSo * b.iMauSo;
+            int niTuSo = a.iTuSo *b.iTuSo;
+            PhanSo kq = new PhanSo(niTuSo, niMauSo);
+            kq.RutGon();
+            return kq;
+        }
+
+        public static PhanSo operator /(PhanSo a, PhanSo b)
+        {
+            if (b.iTuSo == 0)
+            {
+                throw new DivideByZeroException("Khong the chia cho phan so bang 0 (tu so bang 0)");
+            }
+
+            int niTuSo = a.iTuSo * b.iMauSo;
+            int niMauSo = a.iMauSo * b.iTuSo;
+            PhanSo kq = new PhanSo(niTuSo, niMauSo);
+            kq.RutGon();
+            return kq;
+        }
+
+
+        public double GetValue()
+        {
+            return iTuSo*1.0 / iMauSo;
+        }
+
+        public int CompareTo(PhanSo other)
+        {
+            if(GetValue() < other.GetValue())
+                return -1;
+            else if (GetValue() > other.GetValue())
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ bai04/Program.cs
@@ -0,0 +1,170 @@
+﻿using bai04;
+using System;
+using System.Numerics;
+
+public  class dsPhanSo
+{
+    private List<PhanSo> listPs = new List<PhanSo>();
+    public void Nhap()
+    {
+        listPs.Clear();
+        int n;
+        while (true) 
+        { 
+        Console.Write("Nhap so luong phan so cua day: ");
+        string str=Console.ReadLine();
+            if (int.TryParse(str, out n) && n > 0)
+            {
+                break;
+            }
+            Console.WriteLine("Nhap so luong phan so hop le! So luong phan so phai lon hon 0 va la so nguyen!");
+        }
+        Console.WriteLine();
+        for (int i = 0; i < n; i++)
+        {
+            PhanSo p = new PhanSo();
+            p.Nhap();
+            listPs.Add(p);
+            Console.WriteLine();
+        }
+    }
+
+    public void Xuat()
+    {
+        for (int i = 0; i < listPs.Count; i++)
+        {
+            listPs[i].Xuat();
+        }
+        Console.WriteLine();
+    }
+
+    public void TimPhanSoLonNhat()
+    {
+        double maxVal=listPs[0].GetValue();
+        for (int i = 1; i < listPs.Count; i++)
+        {
+            if(maxVal< listPs[i].GetValue())
+            {
+                maxVal = listPs[i].GetValue();
+            }
+        }
+
+        bool exist = false;
+        for (int i=0; i < listPs.Count; i++)
+        {
+            if(listPs[i].GetValue() == maxVal)
+            {
+                if(exist == false)
+                    Console.Write("Phan so lon nhat la: ");
+                exist = true;                
+                listPs[i].Xuat();
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+      
+    public void Sort()
+    {
+        listPs.Sort();
+    }
+}
+public class Program
+{
+    public void TinhToan()
+    {
+        PhanSo ps1 = new PhanSo();
+        PhanSo ps2 = new PhanSo();
+
+        
+        Console.WriteLine("\nNhap phan so thu nhat");
+        ps1.Nhap();
+
+        Console.WriteLine("\nNhap phan so thu hai");
+        ps2.Nhap();
+
+        PhanSo pskq;
+
+        Console.Write("Tong hai phan so: ");
+        pskq = ps1 + ps2;
+        pskq.Print();
+
+        Console.Write("\nHieu hai phan so: ");
+        pskq = ps1 - ps2;
+        pskq.Print();
+
+        Console.Write("\nTich hai phan so: ");
+        pskq = ps1 * ps2;
+        pskq.Print();
+
+        Console.Write("\nThuong hai phan so: ");
+        try
+        {
+            pskq = ps1 / ps2;
+            pskq.Print();
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine($"Loi: {ex.Message}");
+        }
+
+    }
+
+    public static void Main()
+    {
+        dsPhanSo dsPhanSo = new dsPhanSo();
+        dsPhanSo.Nhap();
+
+        int choice = -1;
+        while (choice != 0)
+        {
+            Console.WriteLine("\n======== Menu ========");
+            Console.WriteLine("1. Xuat danh sach phan so");
+            Console.WriteLine("2. Tim phan so lon nhat");
+            Console.WriteLine("3. Sap xep tang dan");
+            Console.WriteLine("4. Nhap lai danh sach phan so");
+            Console.WriteLine("5. Tinh toan voi hai phan so");
+            Console.WriteLine("0. Thoat");
+            Console.Write("Nhap lua chon: ");
+            string input;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (int.TryParse(input, out choice) && choice >= 0 && choice <= 5)
+                    break;
+                else
+                    Console.Write("Lua chon khong hop le! Nhap lai: ");
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Danh sach phan so: ");
+                    dsPhanSo.Xuat();
+                    break;
+                case 2:
+                    dsPhanSo.TimPhanSoLonNhat();
+                    break;
+                case 3:
+                    dsPhanSo.Sort();
+                    Console.Write("Danh sach sau khi sap xep tang dan: ");
+                    dsPhanSo.Xuat();
+                    break;
+                case 4:
+                    dsPhanSo.Nhap();
+                    break;
+                case 5:
+                    Program program = new Program();
+                    program.TinhToan();
+                    break;
+                case 0:
+                    Console.WriteLine("Thoat chuong trinh...");
+                    break;
+                default:
+                    Console.WriteLine("Lua chon khong hop le!");
+                    break;
+            }
+        }
+    }
+
+}
