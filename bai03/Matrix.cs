874b574513e4d4c67a7fd661055b635cf63830b1
Nguyễn Ái My <24521092@gm.uit.edu.vn>
2025-10-30 22:57:53 +0700

    Add files via upload

---
+++ bai03/Matrix.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai03
+{
+    internal class Matrix
+    {
+        private int rows;
+        private int cols;
+        private int[,] data;
+        public Matrix(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            data = new int[rows, cols];
+        }
+        
+        public void Nhap()
+        {
+            string sm, sn;
+            while (true)
+            {
+                Console.Write("Nhap so hang cua ma tran: ");
+                sm = Console.ReadLine();
+
+                if (int.TryParse(sm, out int m) && m > 0)
+                {
+                    break;
+                }
+                Console.WriteLine("So hang khong hop le! Vui long nhap lai!");
+            }
+
+            while (true)
+            {
+                Console.Write("Nhap so cot cua ma tran: ");
+                sn = Console.ReadLine();
+                if (int.TryParse(sn, out int n) && n > 0)
+                {
+                    break;
+                }
+                Console.WriteLine("So cot khong hop le! Vui long nhap lai!");
+            }
+            rows= int.Parse(sm);
+            cols= int.Parse(sn);
+
+            data = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"Nhap phan tu [{i},{j}]: ");
+                    data[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        public void Xuat()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(data[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void TimKiem(int x)
+        {
+            bool found = false;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (data[i, j] == x)
+                    {
+                        Console.WriteLine($"Phan tu can tim o hang {i+1}, cot {j+1}");
+                        found = true;
+                    }
+                }
+            }
+            if (!found)
+                Console.WriteLine("Khong tim thay phan tu trong ma tran.");
+        }
+
+        bool IsPrime(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+
+        public void XuatPhanTuLaSoNguyenTo()
+        {
+            List<int> primes = new List<int>();
+           
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (IsPrime(data[i, j]))
+                    {
+                        primes.Add(data[i,j]);
+                    }
+                }
+            }
+
+            if (primes.Count == 0)
+            {
+                Console.WriteLine("Khong co so nguyen to trong ma tran.");
+                return;
+            }
+            else
+            {
+                Console.Write("Cac phan tu la so nguyen to trong ma tran:");
+                foreach (var prime in primes)
+                {
+                    Console.Write(prime + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void TimDongCoNhieuSoNguyenToNhat()
+        {
+            int maxCount = 0;
+            List<int> rowIndices = new List<int>();
+
+            for (int i = 0; i < rows; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < cols; j++)
+                {
+                    if (IsPrime(data[i, j]))
+                    {
+                        count++;
+                    }
+                }
+
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    rowIndices.Clear();
+                    rowIndices.Add(i); 
+                }
+                else if (count == maxCount && count > 0)
+                {
+                    rowIndices.Add(i);
+                }
+            }
+
+            if (rowIndices.Count > 0)
+            {
+                Console.WriteLine($"(Dong co nhieu so nguyen to nhat (voi {maxCount} so nguyen to):");
+                foreach (int index in rowIndices)
+                {
+                    Console.WriteLine($"- Dong {index + 1}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Khong co so nguyen to trong ma tran.");
+            }
+        }
+
+    }
+}
---
+++ bai03/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using bai03;
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        Matrix matrix = new Matrix(0, 0);
+        matrix.Nhap();
+
+        int choice = -1;
+        while (choice != 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine("=========Menu==========");
+            Console.WriteLine("1. Xuat ma tran");
+            Console.WriteLine("2. Tim kiem phan tu");
+            Console.WriteLine("3. Xuat cac phan tu la so nguyen to");
+            Console.WriteLine("4. Tim dong co nhieu so nguyen to nhat");
+            Console.WriteLine("5. Nhap lai ma tran");
+            Console.WriteLine("0. Thoat");
+            Console.Write("Nhap lua chon cua ban: ");
+            string input;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (int.TryParse(input, out choice) && choice > 0 && choice <= 5)
+                    break;
+                else
+                    Console.Write("Lua chon khong hop le! Nhap lai: ");
+            }
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Ma tran vua nhap:");
+                    matrix.Xuat();
+                    break;
+                case 2:
+                    Console.Write("Nhap phan tu can tim: ");
+                    int x = int.Parse(Console.ReadLine());
+                    matrix.TimKiem(x);
+                    break;
+                case 3:
+                    matrix.XuatPhanTuLaSoNguyenTo();
+                    break;
+                case 4:
+                    matrix.TimDongCoNhieuSoNguyenToNhat();
+                    break;
+                case 5:
+                    matrix.Nhap();
+                    break;
+                case 0:
+                    Console.WriteLine("Thoat chuong trinh.");
+                    break;
+                default:
+                    Console.WriteLine("Lua chon khong hop le!");
+                    break;
+            }
+        }
+    }    
+}
